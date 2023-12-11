c753056cbce5318cb334a2e1fb50629099e69d42
RNBSilva <RNBSilva@P381886>
2023-12-11 17:01:15 -0300

    Create logger classes (implement logs to database)

---
+++ Logging/CustomLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace ApiCatalogoController.Logging
+{
+    public class CustomLoggerProvider : ILoggerProvider
+    {
+        private readonly CustomLoggerProviderConfiguration loggerConfig;
+        private readonly ConcurrentDictionary<string, CustomerLogger> loggers =
+            new ConcurrentDictionary<string, CustomerLogger>();
+
+        public CustomLoggerProvider(CustomLoggerProviderConfiguration configuration)
+        {
+            loggerConfig = configuration;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            // cria uma instância do logger customizado
+            return loggers.GetOrAdd(categoryName, name => new CustomerLogger(name, loggerConfig));
+        }
+
+        public void Dispose()
+        {
+            loggers.Clear();
+        }
+    }
+}
