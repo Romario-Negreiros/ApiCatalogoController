17bfa5b283a535c45be41ea6d854b03220e9cccb
RNBSilva <RNBSilva@P381886>
2023-12-12 08:24:36 -0300

    Correct migration name

--- Migrations/20231211160507_AddProductsAndCategoriesTables.Designer.cs
+++ Migrations/20231211160507_AddProductsAndCategoriesTables.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiCatalogoController.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20231211160507_CreateCategoriesAndProductsTable")]
-    partial class CreateCategoriesAndProductsTable
+    [Migration("20231211160507_AddProductsAndCategoriesTables")]
+    partial class AddProductsAndCategoriesTables
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -22,78 +22,78 @@ namespace ApiCatalogoController.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("ApiCatalogoController.Models.Category", b =>
-                {
-                    b.Property<int>("CategoryId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+            {
+                b.Property<int>("CategoryId")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
 
-                    b.Property<string>("Description")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+                b.Property<string>("Description")
+                    .IsRequired()
+                    .HasColumnType("longtext");
 
-                    b.Property<string>("ImageUrl")
-                        .HasColumnType("longtext");
+                b.Property<string>("ImageUrl")
+                    .HasColumnType("longtext");
 
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+                b.Property<string>("Name")
+                    .IsRequired()
+                    .HasColumnType("longtext");
 
-                    b.HasKey("CategoryId");
+                b.HasKey("CategoryId");
 
-                    b.ToTable("Categories");
-                });
-
-            modelBuilder.Entity("ApiCatalogoController.Models.Product", b =>
-                {
-                    b.Property<int>("ProductId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    b.Property<int>("CategoryId")
-                        .HasColumnType("int");
-
-                    b.Property<string>("Description")
-                        .IsRequired()
-                        .HasColumnType("longtext");
-
-                    b.Property<string>("ImageUrl")
-                        .HasColumnType("longtext");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("longtext");
-
-                    b.Property<decimal>("Price")
-                        .HasColumnType("decimal(65,30)");
-
-                    b.Property<DateTime>("RegistrationDate")
-                        .HasColumnType("datetime(6)");
-
-                    b.Property<int>("Stock")
-                        .HasColumnType("int");
-
-                    b.HasKey("ProductId");
-
-                    b.HasIndex("CategoryId");
-
-                    b.ToTable("Products");
-                });
+                b.ToTable("Categories");
+            });
 
             modelBuilder.Entity("ApiCatalogoController.Models.Product", b =>
-                {
-                    b.HasOne("ApiCatalogoController.Models.Category", "Category")
-                        .WithMany("Products")
-                        .HasForeignKey("CategoryId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+            {
+                b.Property<int>("ProductId")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
 
-                    b.Navigation("Category");
-                });
+                b.Property<int>("CategoryId")
+                    .HasColumnType("int");
+
+                b.Property<string>("Description")
+                    .IsRequired()
+                    .HasColumnType("longtext");
+
+                b.Property<string>("ImageUrl")
+                    .HasColumnType("longtext");
+
+                b.Property<string>("Name")
+                    .IsRequired()
+                    .HasColumnType("longtext");
+
+                b.Property<decimal>("Price")
+                    .HasColumnType("decimal(65,30)");
+
+                b.Property<DateTime>("RegistrationDate")
+                    .HasColumnType("datetime(6)");
+
+                b.Property<int>("Stock")
+                    .HasColumnType("int");
+
+                b.HasKey("ProductId");
+
+                b.HasIndex("CategoryId");
+
+                b.ToTable("Products");
+            });
+
+            modelBuilder.Entity("ApiCatalogoController.Models.Product", b =>
+            {
+                b.HasOne("ApiCatalogoController.Models.Category", "Category")
+                    .WithMany("Products")
+                    .HasForeignKey("CategoryId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
+
+                b.Navigation("Category");
+            });
 
             modelBuilder.Entity("ApiCatalogoController.Models.Category", b =>
-                {
-                    b.Navigation("Products");
-                });
+            {
+                b.Navigation("Products");
+            });
 #pragma warning restore 612, 618
         }
     }
